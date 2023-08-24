4910884abc104b3ac4f224e36e0bbaa1ac5c4298
Finbarr Howard <finbarr.o.c.howard@gmail.com>
2023-08-24 15:34:18 +1000

    heaps of stuff

--- server/config/config.py
+++ server/config/config.py
@@ -1,4 +1,4 @@
-from config.secrets import IT_PROJECT_MONGO_PASSWORD, IT_PROJECT_MONGO_USERNAME
+# # from config.secrets import IT_PROJECT_MONGO_PASSWORD, IT_PROJECT_MONGO_USERNAME
 
-DATABASE_URI = f"mongodb+srv://{IT_PROJECT_MONGO_USERNAME}:{IT_PROJECT_MONGO_PASSWORD}@cluster0.c1418is.mongodb.net/?retryWrites=true&w=majority"
-DATABASE_NAME = "it_project"
+# DATABASE_URI = f"mongodb+srv://{IT_PROJECT_MONGO_USERNAME}:{IT_PROJECT_MONGO_PASSWORD}@cluster0.c1418is.mongodb.net/?retryWrites=true&w=majority"
+# DATABASE_NAME = "it_project"
